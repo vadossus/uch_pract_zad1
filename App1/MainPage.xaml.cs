973cab871e760bd0d7f768d8f543a78d0c76d34b
vadossus <vadim.gorowoy2015@gmail.com>
2024-12-04 17:40:05 +0500

    Main

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+
+    }
+}
